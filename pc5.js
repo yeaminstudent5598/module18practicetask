99339dcdadcb69497597365b48e941ce7181e941
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2024-08-31 16:25:42 +0600

    practice five done

---
+++ pc5.js
@@ -0,0 +1,13 @@
+let num1 = 10;
+let num2 = 5; 
+let result = 0;
+
+if (num1 > num2) {
+    result = num1 * 2;
+    console.log('result', result)
+} else {
+    result = num1 + num2
+    console.log('result', result)
+}
+
+
